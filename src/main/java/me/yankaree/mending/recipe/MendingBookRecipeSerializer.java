415ca20eab4ca83a9f6f887b4f499c285f6d78aa
Yankaree <sngc03769@gmail.com>
2026-06-30 22:00:07 -0400

    again

--- src/main/java/me/yankaree/mending/recipe/MendingBookRecipeSerializer.java
+++ src/main/java/me/yankaree/mending/recipe/MendingBookRecipeSerializer.java
@@ -2,53 +2,46 @@ package me.yankaree.mending.recipe;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.mojang.datafixers.util.Pair;
 
 import net.minecraft.core.NonNullList;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.resources.Identifier;
-import net.minecraft.util.GsonHelper;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 
-public class MendingBookRecipeSerializer implements RecipeSerializer<MendingBookRecipe> {
+public class MendingBookRecipeSerializer extends RecipeSerializer<MendingBookRecipe> {
 	public static final MendingBookRecipeSerializer INSTANCE = new MendingBookRecipeSerializer();
 
-	private MendingBookRecipeSerializer() {
-	}
-
-	@Override
 	public MendingBookRecipe fromJson(Identifier id, JsonObject json) {
-		NonNullList<Ingredient> ingredients = readIngredients(json);
-		return new MendingBookRecipe(ingredients);
-	}
-
-	@Override
-	public MendingBookRecipe fromNetwork(Identifier id, FriendlyByteBuf buf) {
-		int size = buf.readVarInt();
-		NonNullList<Ingredient> ingredients = NonNullList.withSize(size, Ingredient.EMPTY);
-		for (int i = 0; i < size; i++) {
-			ingredients.set(i, Ingredient.fromNetwork(buf));
-		}
-		return new MendingBookRecipe(ingredients);
-	}
-
-	@Override
-	public void toNetwork(FriendlyByteBuf buf, MendingBookRecipe recipe) {
-		buf.writeVarInt(recipe.getIngredients().size());
-		for (Ingredient ingredient : recipe.getIngredients()) {
-			ingredient.toNetwork(buf);
-		}
-	}
-
-	private static NonNullList<Ingredient> readIngredients(JsonObject json) {
-		JsonArray jsonArray = GsonHelper.getAsJsonArray(json, "ingredients");
-		NonNullList<Ingredient> ingredients = NonNullList.create();
+		NonNullList<Ingredient> ingredients = new NonNullList<>(Ingredient.class, 9);
+		JsonArray jsonArray = json.getAsJsonArray("ingredients");
 		for (int i = 0; i < jsonArray.size(); i++) {
-			Ingredient ingredient = Ingredient.fromJson(jsonArray.get(i));
+			Ingredient ingredient = Ingredient.CODEC.parse(jsonArray.get(i));
 			if (!ingredient.isEmpty()) {
 				ingredients.add(ingredient);
 			}
 		}
-		return ingredients;
+		if (ingredients.isEmpty()) {
+			return null;
+		}
+		return new MendingBookRecipe(ingredients);
+	}
+
+	public MendingBookRecipe fromNetwork(FriendlyByteBuf buf) {
+		int size = buf.readVarInt();
+		NonNullList<Ingredient> ingredients = new NonNullList<>(Ingredient.class, size);
+		for (int i = 0; i < size; i++) {
+			ingredients.add(Ingredient.CODEC.decode(buf.readWithCodec(Ingredient.CODEC)));
+		}
+		return new MendingBookRecipe(ingredients);
+	}
+
+	public void toNetwork(FriendlyByteBuf buf, MendingBookRecipe recipe) {
+		buf.writeVarInt(recipe.getIngredients().size());
+		for (Ingredient ingredient : recipe.getIngredients()) {
+			buf.writeWithCodec(Ingredient.CODEC, ingredient);
+		}
 	}
 }
